00b9331d2f60f3200bffc6c19979ec5c0b800cb7
Laszlo Teveli <tevelee@gmail.com>
2024-02-22 14:39:15 +0100

    Initial Commit

---
+++ DB/DBApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBApp.swift
+//  DB
+//
+//  Created by Laszlo Teveli on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
